5c5a400e3a4ea005f0491bbc1bc67f96ede1f7fe
DrDeee <63226814+DrDeee@users.noreply.github.com>
2020-11-19 19:00:34 +0100

    Added pkg

--- main.js
+++ main.js
@@ -1,39 +1,34 @@
 const { Client } = require('discord-rpc')
 const path = require('path')
 const fetch = require('node-fetch')
-const ms = require('./minestats');
+const ms = require('./minestats')
 
-let data
-let colorCodes = ['\u00A70', '\u00A71', '\u00A72', '\u00A73', '\u00A74', '\u00A75', '\u00A76', '\u00A77']
-ms.init('mc-nexus.de', 25565, function (result) {
-    console.log("Minecraft server status of " + ms.address + " on port " + ms.port + ":");
-    if (ms.online) {
-        data = {
-            state: 'Online (' + ms.version + "): " + ms.current_players + " von " + ms.max_players + " Spieler",
-            details: ms.motd.split("\n")[1].replace(/(\u00a7[0-9A-Fa-f])/g, '')
-        }
+const clientId = '750725647495856149';
+
+const rpc = new Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+const arr = new Array(0, 1, 2, 3, 4, 5, 6, 7)
+const item = arr[Math.floor(Math.random() * arr.length)].toString()
+
+async function setActivity() {
+    if (!rpc) {
+        return;
     }
-    else {
-        data = {
-            details: 'Mc-Nexus.de | Deine Minecraft-Community',
-            state: 'Offline'
+    let data
+    ms.init('mc-nexus.de', 25565, function (result) {
+        if (ms.online) {
+            data = {
+                state: 'Online (' + ms.version + "): " + ms.current_players + " von " + ms.max_players + " Spieler",
+                details: ms.motd.split("\n")[1].substr(3)
+            }
         }
-    }
-    console.log(data.details)
-    const clientId = '750725647495856149';
-
-    const rpc = new Client({ transport: 'ipc' });
-    const startTimestamp = new Date();
-
-    const arr = new Array(0, 1, 2, 3, 4, 5, 6, 7)
-    const item = arr[Math.floor(Math.random() * arr.length)].toString()
-
-    async function setActivity() {
-        if (!rpc) {
-            return;
+        else {
+            data = {
+                details: 'Mc-Nexus.de | Deine Minecraft-Community',
+                state: 'Offline'
+            }
         }
-        // You'll need to have snek_large and snek_small assets uploaded to
-        // https://discord.com/developers/applications/<application_id>/rich-presence/assets
         rpc.setActivity({
             details: data.details,
             state: data.state,
@@ -44,17 +39,21 @@ ms.init('mc-nexus.de', 25565, function (result) {
             smallImageText: 'MC-Nexus.de - Logo',
             instance: false,
         });
-    }
-
-    rpc.on('ready', () => {
-        setActivity();
-    });
-
-    rpc.login({ clientId }).catch(console.error);
-
-    process.on('exit', () => {
-        rpc.destroy()
     })
+    // You'll need to have snek_large and snek_small assets uploaded to
+    // https://discord.com/developers/applications/<application_id>/rich-presence/assets
+    
+}
 
+rpc.on('ready', () => {
+    setActivity();
 });
+
+rpc.login({ clientId }).catch(console.error);
+
+process.on('exit', () => {
+    rpc.destroy()
+})
+
+setInterval(setActivity, 60 * 1000)
 
